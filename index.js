3403ad70f05cd0d3808e43857a52f6b1e99a9215
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-09-13 10:43:30 +0500

    optimal

--- index.js
+++ index.js
@@ -210,3 +210,15 @@
 // Convert boolean values to strings 'Yes' or 'No'.
 // JavaScript:
 // const boolToWord = ( bool )=> bool  ? "Yes":"No"
+
+// function getCount(str) {
+//   let vowels = ['a', 'e', 'i', 'o', 'u'];
+//   var vowelsCount = 0;
+  
+//   // enter your majic here
+//   for (let i of str) {
+//     if (vowels.includes(i)) vowelsCount++;
+//   }
+  
+//   return vowelsCount;
+// }
